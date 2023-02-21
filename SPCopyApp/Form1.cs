13ff591824b76a43c2547e8d06b25c005b0e0e82
Юрий Иванов <jura401@mail.ru>
2023-02-21 23:30:20 +0300

    2102

--- SPCopyApp/Form1.cs
+++ SPCopyApp/Form1.cs
@@ -9,8 +9,8 @@ namespace SPCopyApp
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            tb_originPath.Text = Directory.GetCurrentDirectory();
-            tb_targetPath.Text = Directory.GetCurrentDirectory();
+            tb_originPath.Text = @"D:\text.txt";
+            tb_targetPath.Text = @"D:\textcopy.txt";
         }
 
         private void btn_openFile_Click(object sender, EventArgs e)
@@ -31,11 +31,24 @@ namespace SPCopyApp
         private void btn_start_Click(object sender, EventArgs e)
         {
             FileStream fsopen = new FileStream(tb_originPath.Text, FileMode.Open);
-            byte[] data = new byte[5];
-            fsopen.Read (data, 0, 5);
-            fsopen.Close();
             FileStream fswrite = new FileStream(tb_targetPath.Text, FileMode.OpenOrCreate);
-            fswrite.Write(data, 0, 5);
+            int b;
+            long lengthfile = fsopen.Length;
+            int countOperation = 3;
+            
+            int[] partfilelength = new int[countOperation];
+            for(int i = 0; i < countOperation; i++)
+            {
+                partfilelength[i] =  (int)lengthfile / countOperation;
+                if (i == countOperation - 1)
+                    partfilelength[i] += (int)lengthfile % countOperation;
+            }
+            
+            while ((b = fsopen.ReadByte()) != -1)
+            {
+                fswrite.WriteByte((byte)b);
+            }
+            fsopen.Close();
             fswrite.Close();
 
         }

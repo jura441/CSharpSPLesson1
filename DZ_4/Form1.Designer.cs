df670eec2fa61cb981f7d3c648bcc9c26ed92cec
Юрий Иванов <jura401@mail.ru>
2023-02-26 22:28:38 +0300

    2602

---
+++ DZ_4/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace DZ_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.progressBar5 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(788, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(0, 29);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(788, 23);
+            this.progressBar2.TabIndex = 1;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(0, 157);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(788, 23);
+            this.progressBar3.TabIndex = 2;
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(0, 111);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(788, 23);
+            this.progressBar4.TabIndex = 3;
+            // 
+            // progressBar5
+            // 
+            this.progressBar5.Location = new System.Drawing.Point(0, 67);
+            this.progressBar5.Name = "progressBar5";
+            this.progressBar5.Size = new System.Drawing.Size(788, 23);
+            this.progressBar5.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar5);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.ProgressBar progressBar5;
+    }
+}
+
---
+++ DZ_4/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.Threading;
+
+namespace DZ_4
+{
+    public partial class Form1 : Form
+    {
+        List<ResultRow> results = new List<ResultRow>();
+        List<MyTimer> timers = new List<MyTimer>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (Control control in this.Controls)
+            {
+                if (control != null && control.GetType() == progressBar1.GetType())
+                {
+                    ProgressBar bar = (ProgressBar)control;
+                    timers.Add(new MyTimer(bar));
+                }
+            }
+
+            foreach (MyTimer timer in timers)
+            {
+                timer.startTimer();
+            }
+
+            Thread thread = new Thread(
+                new ThreadStart(
+                        delegate ()
+                        {
+                            bool notend = true;
+                            int counter = 0;
+                            while (notend)
+                            {
+                                foreach (MyTimer timer in timers)
+                                {
+                                    if (!timer.isRunning)
+                                    {
+                                        counter++;
+                                    }
+                                    if (counter == 5)
+                                    {
+                                        notend = false;
+                                    }
+                                    else
+                                    {
+                                        counter = 0;
+                                    }
+                                }
+                                Thread.Sleep(200);
+                            }
+                            foreach (MyTimer timer in timers)
+                            {
+                                results.Add(timer.getResultTimers());
+                            }
+                        }
+                    )
+                );
+            thread.Start();
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string res = "";
+            foreach (ResultRow result in results)
+            {
+                res += result.ToString() + "\n";
+            }
+            MessageBox.Show(res);
+        }
+    }
+    class MyTimer
+    {
+        public bool isRunning = false;
+        System.Threading.Timer timer;
+        ProgressBar _progress;
+        DateTime time;
+        TimeSpan span = TimeSpan.Zero;
+        public MyTimer(ProgressBar progress)
+        {
+            _progress = progress;
+        }
+
+        public void startTimer()
+        {
+            isRunning = true;
+            time = DateTime.Now;
+            TimerCallback callback = new TimerCallback(this.CallbackDel);
+            timer = new System.Threading.Timer(callback, _progress, 0, 500);
+        }
+        void deleteTimer()
+        {
+            span = DateTime.Now - time;
+            isRunning = false;
+            timer.Dispose();
+        }
+        void CallbackDel(object obj)
+        {
+            ProgressBar bar = (ProgressBar)obj;
+            bar.BeginInvoke(new Action(() => {
+                Random random = new Random();
+                int step = random.Next(0, 5);
+                if (bar.Value + step > bar.Maximum)
+                {
+                    bar.Value = bar.Maximum;
+                    deleteTimer();
+                }
+                else
+                    bar.Value += step;
+            }));
+
+        }
+        public ResultRow getResultTimers()
+        {
+            return new ResultRow(_progress.Name, span);
+        }
+
+    }
+
+    class ResultRow
+    {
+        string BarName;
+        TimeSpan span;
+
+        public ResultRow(string name, TimeSpan time)
+        {
+            BarName = name;
+            span = time;
+        }
+        public override string ToString()
+        {
+            return BarName + " " + span.ToString();
+        }
+    }
+}

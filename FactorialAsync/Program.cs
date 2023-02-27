8a5ddb2a7cf2d937751e784494add65001f85fab
Юрий Иванов <jura401@mail.ru>
2023-02-27 18:57:44 +0300

    2702

---
+++ FactorialAsync/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(Factorial(5).ToString());
+
+static int Factorial(int n)
+{
+    int res = 1;    
+    for (int i = n; i > 0; i--)
+    {
+        res *= i;
+
+    }
+    return res;
+}

9e3cd0a64fc753458c26a9c3e8132e30917e13ef
Юрий Иванов <jura401@mail.ru>
2023-02-27 21:21:24 +0300

    270223

---
+++ Exam/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+//Console.WriteLine("Hello, World!");
+string[] badWords = new string[] { "экзамен", "Экзамен"};
+
+string orignPath = @"D:\Exam";
+DirectoryInfo info = new DirectoryInfo(orignPath);
+List<FileInfo> list = new List<FileInfo>();
+List<DirectoryInfo> list2 = new List<DirectoryInfo>();
+
+
+foreach (FileInfo file in list)
+{
+    Console.WriteLine(file.Name);
+
+}
+foreach (DirectoryInfo directory in list2)
+{
+    Console.WriteLine(directory.FullName)
+}
+foreach (FileInfo file in list)
+
--- FactorialAsync/Program.cs
+++ FactorialAsync/Program.cs
@@ -1,6 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine(Factorial(5).ToString());
+static void Main(string[] args)
+{
+    Console.WriteLine(Factorial(5).ToString());
+    // поточный метод
+    Thread thread = new Thread(new ParameterizedThreadStart(FactorialAsync));
+    thread.Start(5);
+
+    Thread threadStep = new Thread(new ParameterizedThreadStart(StepenAsync));
+    threadStep.Start(new MyPow(5,3));
+}
+
+
+
+static void FactorialAsync(object obj)
+{
+    obj = Factorial((int)obj);
+    Console.WriteLine(((int)obj).ToString());
+}
+
+static void StepenAsync(object obj)
+{
+    MyPow pow = (MyPow)obj;
+    Console.WriteLine(pow.Getresult().ToString());
+}
+
 
 static int Factorial(int n)
 {
@@ -11,4 +35,27 @@ static int Factorial(int n)
 
     }
     return res;
+}
+
+class MyPow
+{
+    int _number;
+    int _stepen;
+
+    public MyPow (int number, int stepen)
+    {
+        _number = number;
+        _stepen = stepen;
+
+    }
+
+    public int Getresult()
+    {
+        int result = 1;
+        for(int i=0; i <_stepen; i++)
+        {
+            result *= _number;
+        }
+        return result;
+    }
 }
